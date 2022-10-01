15a9fc19e18b31aa8bf0903dbb3ef5b26b814841
Alberto Calleros <jatcalleros@gmail.com>
2022-09-30 18:28:01 -0600

    finish practica de for 8

---
+++ pratica_de_for_8.py
@@ -0,0 +1,12 @@
+# Escriba un programa para imprimir todos los numeros
+# primos entre 0 y 100 e imprima cuantos numeros primos hay
+
+count = 0
+print("All of the odd numbers between 0 and 100 are: \n")
+for i in range(0, 100):
+    if i % 2 != 0:
+        print(i, end=" ")
+    if i % 2 != 0:
+        count += 1
+print("\n", "*  "*30)
+print("There are {} numbers in this series".format(count))
