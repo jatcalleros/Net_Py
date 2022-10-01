9e896ea01536fce00ef9a485ced14c8aa9a06389
Alberto Calleros <jatcalleros@gmail.com>
2022-09-30 22:16:09 -0600

    delete unecessary if statement

--- pratica_de_for_8.py
+++ pratica_de_for_8.py
@@ -6,7 +6,6 @@ print("All of the odd numbers between 0 and 100 are: \n")
 for i in range(0, 100):
     if i % 2 != 0:
         print(i, end=" ")
-    if i % 2 != 0:
         count += 1
 print("\n", "*  "*30)
 print("There are {} numbers in this series".format(count))
